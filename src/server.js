5be2fd3e4e4694e7a5d12c3a9a995fc66ae7afdd
bmq92 <bmq928@gmail.com>
2018-11-22 16:42:38 +0700

    add swagger

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 const app = express()
+const swaggerUi = require('swagger-ui-express')
+const swaggerDoc = require('../swagger.json')
 
 const fileExplore = require('./file-explore')
 const uploadFile = require('./upload-file')
@@ -10,9 +12,11 @@ const downloadFile = require('./download-file')
 // dependency
 app.use(helmet())
 app.use(cors())
-// app.use(uploadFile.middleware())
 
-//router
+//document
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+
+//api router
 app.use('/file-explore', fileExplore.route)
 app.use('/upload', uploadFile.route)
 app.use('/download', downloadFile.route)
