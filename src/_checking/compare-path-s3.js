88e4bd8d465fccfaf60998b251dc9248b7a97c2d
bmq92 <bmq928@gmail.com>
2018-12-11 14:23:11 +0700

    remove console.log

--- src/_checking/compare-path-s3.js
+++ src/_checking/compare-path-s3.js
@@ -3,7 +3,7 @@
 module.exports = (path1, path2) => {
   const item1 = path1.split('/').filter(i => i) //remove empty string
   const item2 = path2.split('/').filter(i => i) //remove empty string
-  console.log({item1, item2})
+  
   for (const i in item1) {
     if(item1[i] !== item2[i]) return false;
   }
