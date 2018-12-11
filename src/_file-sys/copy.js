f2cc9622c0e056bd6478dcab18acd7e55910c926
bmq92 <bmq928@gmail.com>
2018-12-11 13:05:13 +0700

    add copy folder

--- src/_file-sys/copy.js
+++ src/_file-sys/copy.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const path = require('path');
+const pathStat = require('./path-stat');
+const { ncp } = require('ncp');
 const { s3 } = require('../_aws');
 
-const withFs = (from, dest) => {
+//copy file
+const withFsFile = (from, dest) => {
   return new Promise((resolve, reject) => {
     fs
       .createReadStream(from)
@@ -12,24 +15,71 @@ const withFs = (from, dest) => {
   });
 }
 
+
+//copy folder
+const withFsFolder = (from, dest) => {
+  return new Promise((resolve, reject) => {
+    ncp(from, dest, { filter: '' }, (err) => {
+      if (err) return reject(err);
+
+      resolve('done');
+    })
+  })
+}
+
+const withFs = (from, dest) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const stat = await pathStat(from);
+      if (stat.isFile()) return resolve(withFsFile(from, dest))
+
+      resolve(withFsFolder(from, dest));
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
+
 // withFs('/home/bui/Desktop/test/optimize-request.txt', '/home/bui/Desktop/test/inner/optimize-request.txt')
 
 const withS3 = (bucket, from, dest) => {
-  from = path.join(bucket, from)
-  // dest = path.join(bucket, dest)
 
-  return new Promise((resolve, reject) => {
-    const params = {
-      Bucket: bucket,
-      CopySource: from,
-      Key: dest
-    };
+  return new Promise(async (resolve, reject) => {
+    // const params = {
+    //   Bucket: bucket,
+    //   CopySource: from,
+    //   Key: dest
+    // };
 
-    s3.copyObject(params, (err, data) => {
-      if(err) return reject(err);
+    try {
+      const allContents = await s3.listObjects({ Prefix: from, Bucket: bucket }).promise();
+      const allDonePromise = allContents.Contents.map(content => new Promise(async (resolve, reject) => {
+        const parmas = {
+          Bucket: bucket,
+          CopySource: path.join(bucket, content.Key),
+          Key: content.Key.replace(from, dest)
+        }
 
-      resolve(data);
-    })
+        try {
+          const data = await s3.copyObject(parmas).promise();
+          resolve(data);
+        } catch (error) {
+          reject(error);
+        }
+      }))
+
+      await Promise.all(allDonePromise);
+      resolve()
+
+    } catch (error) {
+      reject(error)
+    }
+
+    // s3.copyObject(params, (err, data) => {
+    //   if (err) return reject(err);
+
+    //   resolve(data);
+    // })
 
   });
 }
