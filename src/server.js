1819082a7010a2f34ff3691f5c2ee03035ebd1b4
BuiHoang <bdhoang.ht@gmail.com>
2019-03-28 10:51:40 +0700

    fix minor

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ const helmet = require('helmet');
 const cors = require('cors');
 const app = express();
 const crypto = require('crypto');
+
 function getRandomHash() {
 	const current_date = (new Date()).valueOf().toString();
 	const random = Math.random().toString();
 	return (crypto.createHash('sha1').update(current_date + random).digest('hex'));
 }
+
 // const swaggerUi = require('swagger-ui-express');
 // const swaggerDoc = require('../swagger.json');
 
@@ -19,6 +21,11 @@ const readFile = require('./read-file');
 const action = require('./action');
 const search = require('./search');
 
+app.get('/', (req, res) => {
+	res.send({serverId: serverId});
+});
+app.get('/download', downloadFile.route);
+
 // dependency
 app.use(helmet());
 app.use(cors());
@@ -26,14 +33,10 @@ app.use(cors());
 let serverId = getRandomHash();
 console.log("Server ID ", serverId);
 
-app.get('/', (req, res) => {
-	res.send({serverId: serverId});
-});
 //document
 // app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 //authenticate
-app.get('/download', downloadFile.route);
 const authenticate = require('./authenticate');
 app.use(authenticate());
 
