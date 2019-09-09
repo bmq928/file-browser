ae3137900bb434d6b7912129fcee5c7892c92be6
Hoang Dang <bdhoang.ht@gmail.com>
2019-09-09 17:48:35 +0700

    add download multi files router

--- src/download-file/controller.js
+++ src/download-file/controller.js
@@ -1,31 +1,90 @@
 const config = require('config');
 const rootFolderFs = process.env.STORAGE_ROOT_FOLDER || config.get('rootFolder');
-const { getFile, getPath } = require('../_file-sys');
-
+const {getFile, getPath, readDir} = require('../_file-sys');
+const archiver = require('archiver');
+const fs = require('fs');
+const {s3} = require('../_aws');
 const download = (filePath, options, res) => {
 
-  filePath = getPath(filePath, rootFolderFs, options)
+    filePath = getPath(filePath, rootFolderFs, options);
 
-  return new Promise(async (resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
 
-    if (!filePath) return reject(new Error('file_path is required'));
+        if (!filePath) return reject(new Error('file_path is required'));
 
-    try {
+        try {
 
-      const file = await getFile(filePath, options);
+            const file = await getFile(filePath, options);
 
-      res.setHeader('Content-disposition', 'attachment; filename=' + file.name);
-      res.setHeader('Content-type', file.contentType);
+            res.setHeader('Content-disposition', 'attachment; filename=' + file.name);
+            res.setHeader('Content-type', file.contentType);
 
-      file.body.pipe(res);
+            file.body.pipe(res);
 
-      res.on('end', () => resolve())
-    } catch (error) {
-      reject(error)
-    }
+            res.on('end', () => resolve())
+        } catch (error) {
+            reject(error)
+        }
 
-  })
+    })
 };
 
-module.exports = { download };
+const downloadMultiFiles = function (payload, options, res) {
+    let filePaths = payload.files;
+    return new Promise(async (resolve, reject) => {
+        let downloadFileKeys = [];
+        filePaths = filePaths.map(f => getPath(f, rootFolderFs, options));
+        // fetch all s3 key for download
+        for (let i = 0; i < filePaths.length; i++) {
+            if (filePaths[i].endsWith('/')) {
+                let items = await readDir(filePaths[i].substring(0, filePaths[i].lastIndexOf('/')), {
+                    bucket: options.bucket,
+                    s3: true
+                });
+                items = items.map(item => filePaths[i] + item);
+                items.forEach(item => {
+                    downloadFileKeys.push(item);
+                })
+            } else {
+                downloadFileKeys.push(filePaths[i]);
+            }
+        }
+        // init archive stream
+        let archive = archiver('zip', {
+            zlib: {level: 9}
+        });
+        let fileName = Date.now() + '_' + Math.floor(Math.random() * 100000) + '.zip';
+        res.setHeader('Content-disposition', fileName);
+        res.setHeader('Content-type', 'application/zip, application/octet-stream');
+        archive.pipe(res);
+
+        // push all s3 read stream into archive
+        for (let i = 0; i < downloadFileKeys.length; i++) {
+            let params = {
+                Bucket: options.bucket,
+                Key: downloadFileKeys[i]
+            };
+            try {
+                await s3.headObject(params).promise();
+                let s3Stream = s3.getObject(params).createReadStream();
+                let fileKey = downloadFileKeys[i].substring(downloadFileKeys[i].indexOf(options.dir) + options.dir.length);
+                archive.append(s3Stream && s3Stream, {name: fileKey});
+            } catch (e) {
+                if (e.code === "NotFound") {
+                    console.log("File not found ", downloadFileKeys[i])
+                } else {
+                    console.log("Error ne ", e);
+                }
+            }
+        }
+
+        archive.finalize().then((res) => {
+            console.log('Zip file successfully');
+        }).catch((err) => {
+            console.log('Zip error:', err.message);
+        });
+    })
+};
+
+module.exports = {download, downloadMultiFiles};
 
--- src/download-file/route.js
+++ src/download-file/route.js
@@ -2,27 +2,40 @@ const route = require('express').Router();
 // const path = require('path');
 const controller = require('./controller');
 const config = require('config');
+const bodyParser = require('body-parser');
+route.use(bodyParser.json());
 // const rootFolderFs = config.get('rootFolder');
 const options = {
-	s3: process.env.STORAGE_S3 || config.get('s3'),
-	bucket: process.env.STORAGE_BUCKET || config.get('aws.bucket')
+    s3: process.env.STORAGE_S3 || config.get('s3'),
+    bucket: process.env.STORAGE_BUCKET || config.get('aws.bucket')
 };
 
 route.get('/', async (req, res) => {
-	
-	let filePath = req.query.file_path;
-	// filePath = await checking.validateUrl(filePath, req.decoded);
-	// if(!options.s3) filePath = path.join(rootFolderFs, filePath);
-	
-	try {
-		
-		await controller.download(filePath, options, res)
-		
-	} catch (error) {
-		res.status(400).json({error: error.message})
-	}
-	
-	
+
+    let filePath = req.query.file_path;
+    // filePath = await checking.validateUrl(filePath, req.decoded);
+    // if(!options.s3) filePath = path.join(rootFolderFs, filePath);
+
+    try {
+
+        await controller.download(filePath, options, res)
+
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+
+
+});
+
+route.post('/', async (req, res) => {
+    try {
+        await controller.downloadMultiFiles(req.body, {
+            ...options,
+            dir: req.decoded.company + "/" + req.decoded.dir
+        }, res)
+    } catch (err) {
+        res.status(400).json({error: err.message});
+    }
 });
 
 module.exports = route;
